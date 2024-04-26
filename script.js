882130feeaef8efe6f5a26ca7682b2ba9b6dd259
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-27 00:48:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+for (let i = 0; i < str.length; i++) {
+        let repeated = false;
+        for (let j = 0; j < str.length; j++) {
+            if (i !== j && str[i] === str[j]) {
+                repeated = true;
+                break;
+            }
+        }
+        if (!repeated) {
+            return str[i];
+        }
+    }
+    return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
